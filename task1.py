ebd7428f17ef89e26d4369fb72e10494bf2f14f7
sonunegi04 <sonu04120066@gmail.com>
2025-10-23 12:42:32 +0530

    Second Task

---
+++ task1.py
@@ -0,0 +1,6 @@
+x=int(input("Enter the first number: "))
+y=int(input("Enter the second number: "))
+print("Addition: ",x+y)
+print("Subtraction: ",x-y)
+print("Multiplication: ",x*y)
+print("Division: ",x/y)
